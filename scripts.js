482c61efe69af50ae1c7f16f8b365a7ec79afa0b
Gyan Lakhwani <gyanlakhwani@gmail.com>
2018-08-11 17:20:23 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
  function showEnglish() {
-  document.getElementById("englishText").style.display = "inline";
+  document.getElementById("englishText").style.display = "block";
   document.getElementById("hindiText").style.display = "none";
   document.getElementById("marathiText").style.display = "none";
 }
     
 function showHindi() {
   document.getElementById("englishText").style.display = "none";
-  document.getElementById("hindiText").style.display = "inline";
+  document.getElementById("hindiText").style.display = "block";
   document.getElementById("marathiText").style.display = "none";
 }
 
  function showMarathi() {
   document.getElementById("englishText").style.display = "none";
   document.getElementById("hindiText").style.display = "none";
-  document.getElementById("marathiText").style.display = "inline";
+  document.getElementById("marathiText").style.display = "block";
 }
