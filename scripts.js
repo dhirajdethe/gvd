7b8d358724c4e18c9f92b1ea3222ac62fae2aefd
Gyan Lakhwani <gyanlakhwani@gmail.com>
2018-08-11 17:00:59 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+ function showEnglish() {
+  document.getElementById("englishText").style.display = "inline";
+  document.getElementById("hindiText").style.display = "none";
+  document.getElementById("marathiText").style.display = "none";
+}
+    
+function showHindi() {
+  document.getElementById("englishText").style.display = "none";
+  document.getElementById("hindiText").style.display = "inline";
+  document.getElementById("marathiText").style.display = "none";
+}
+
+ function showMarathi() {
+  document.getElementById("englishText").style.display = "none";
+  document.getElementById("hindiText").style.display = "none";
+  document.getElementById("marathiText").style.display = "inline";
+}
